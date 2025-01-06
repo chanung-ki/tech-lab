f78caae4622c9cdd76f50ac6a63816dab884c408
chanung-ki <117616574+chanung-ki@users.noreply.github.com>
2025-01-07 01:19:09 +0900

    [feat] fix cors error

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
+
+const axiosInstance = axios.create({
+  baseURL: "http://localhost:8000",
+});
 
 function App() {
+
+  const handleClick = async () => {
+    try {
+      const response = await axiosInstance.get("/api/test");
+      if (response.status === 200) {
+        console.log(response.data);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div onClick={handleClick}>
+          api 호출
+        </div>
       </header>
     </div>
   );
