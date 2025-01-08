5bf2a4b5c7e63d6d330ab7e42971a74b089ebb28
chanung-ki <117616574+chanung-ki@users.noreply.github.com>
2025-01-08 23:06:20 +0900

    [feat] delete myapp add user app

---
+++ backend/user/models.py
@@ -0,0 +1,42 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.db import models
+
+class TimeModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    class Meta:
+        abstract = True
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        """
+        Creates and returns a superuser with an email and password.
+        """
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        return self.create_user(email, password, **extra_fields)
+
+
+class User(AbstractBaseUser, TimeModel):
+    email = models.EmailField(unique=True)
+    name = models.CharField(max_length=10)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'  # 기본 id로 이메일을 사용
+    REQUIRED_FIELDS = ['name']  # 필수 입력 필드
+
+    def __str__(self):
+        return self.email
